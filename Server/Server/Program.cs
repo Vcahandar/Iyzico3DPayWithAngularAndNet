8f921ff205a40ac9a984a0f25f1165a487249f1e
Vcahandar <jahandarmv@code.edu.az>
2024-08-10 00:51:14 +0400

    finish

---
+++ Server/Server/Program.cs
@@ -0,0 +1,40 @@
+using Server.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(cfr =>
+{
+	cfr.AddDefaultPolicy(policy =>
+	{
+		policy.AllowAnyHeader().AllowCredentials().AllowAnyMethod().SetIsOriginAllowed(policy => true);
+	});
+});
+
+builder.Services.AddSignalR();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<PayHub>("/pay-hub");
+
+app.Run();
